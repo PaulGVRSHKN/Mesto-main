b1fa7ca6de9277f32b950214a60d04aa5a91d67b
Paul Govorushkin <democratiya1986@gmail.com>
2021-08-18 16:20:38 +0300

    fix: 1_review

--- scripts/script.js
+++ scripts/script.js
@@ -1,30 +1,29 @@
-let formElement = document.querySelector('.popup__container');
-let nameInput = document.querySelector('.popup__input_type-name');
-let jobInput = document.querySelector('.popup__input_type-job');
+let formElement = document.querySelector('.popup__form');
+let nameInput = document.querySelector('.popup__input_name');
+let jobInput = document.querySelector('.popup__input_job');
 let editButton = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
 let closeButton = document.querySelector('.popup__close-button');
-let profileName = document.querySelector('.profile__text_name');
-let profileJob = document.querySelector('.profile__text_job');
+let profileName = document.querySelector('.profile__name');
+let profileJob = document.querySelector('.profile__job');
 
-let popupOpen = () => {
+let openPopup = () => {
     popup.classList.add('popup_active');
-}
-let popupClose = () => {
-    popup.classList.remove('popup_active');
-}
-let formSubmitHandler = evt => {
-    evt.preventDefault();
-    profileName.textContent = nameInput.value;
-    profileJob.textContent = jobInput.value;
-    popupClose();
-}
-let editProfile = () => {
-    popupOpen();
     nameInput.value = profileName.textContent;
     jobInput.value = profileJob.textContent;
 }
 
+let closePopup = () => {
+    popup.classList.remove('popup_active');
+}
+
+let formSubmitHandler = evt => {
+    evt.preventDefault();
+    profileName.textContent = nameInput.value;
+    profileJob.textContent = jobInput.value;
+    closePopup();
+}
+
 formElement.addEventListener('submit', formSubmitHandler);
-closeButton.addEventListener('click', popupClose);
-editButton.addEventListener('click', editProfile);
+closeButton.addEventListener('click', closePopup);
+editButton.addEventListener('click', openPopup);
